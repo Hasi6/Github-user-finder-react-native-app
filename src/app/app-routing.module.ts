849c9d13f117347c784b09fac2c571703c3565c0
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-05-18 11:48:48 +0530

    create navbar component and make some changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { ChatComponent } from './pages/chat/chat.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'chat', component: ChatComponent },
   { path: '**', redirectTo: '/loging' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import  { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
+    ChatComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
