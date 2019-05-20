4d8edff8bd8478cbb9780ed5ade1534cdc619c75
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-05-20 09:07:00 +0530

    Update auth.guard.ts

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,38 +1,34 @@
-import { CanActivate } from '@angular/router/src/utils/preactivation';
-import { AuthService } from './../services/auth.service';
-import { AlertService } from './../services/alert.service';
 import { AlertType } from './../enums/alert-type.enum';
 import { Alert } from './../classes/alert';
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { AlertService } from './../services/alert.service';
+import { AuthService } from './../services/auth.service';
 import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router'
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { map, take, tap } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard  implements CanActivate {
 
-  constructor(
-    private auth: AuthService,
-    private router: Router,
-    private alertService: AlertService,
-  ){}
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(
+        private auth: AuthService,
+        private router: Router,
+        private alertService: AlertService,
+      ){}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | boolean {
-      return this.auth.currentUser.pipe(
-        take(1),
-        map((currentUser) => !!currentUser),
-        tap((loggedIn) => {
-          if (!loggedIn) {
-            this.alertService.alerts.next(new Alert('You must be logged in!!', AlertType.Danger));
-            this.router.navigate(['/login'], {queryParams: { returnUrl: state.url}});
-          }
-        })
-      )
-    }
-
-
+      canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean> | boolean {
+          return this.auth.currentUser.pipe(
+            take(1),
+            map((currentUser) => !!currentUser),
+            tap((loggedIn) => {
+              if (!loggedIn) {
+                this.alertService.alerts.next(new Alert('You must be logged in!!', AlertType.Danger));
+                this.router.navigate(['/login'], {queryParams: { returnUrl: state.url}});
+              }
+            })
+          )
+        }
 }
